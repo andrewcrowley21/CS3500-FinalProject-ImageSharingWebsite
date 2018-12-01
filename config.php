5e70f2abdc46ccf7409e902bb7291eaea23b68b9
Andrew Crowley <44073467+andrewcrowley21@users.noreply.github.com>
2018-12-01 00:23:18 -0500

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', '');
-define('DBNAME', 'cs3500_imagedb');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING','mysql:dbname=cs3500_imagedb;charset=utf8mb4;');
-?>
