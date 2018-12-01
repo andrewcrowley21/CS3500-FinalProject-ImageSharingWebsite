ceb64d469d7bdf472970dc0f85ffdadf33a4e3b3
cruemartin92 <crue.c.martin@wmich.edu>
2018-11-30 21:51:33 -0500

    Added required.php

---
+++ required.php
@@ -0,0 +1,18 @@
+<?php
+	require_once('config.php');
+
+	session_start();
+
+	try{
+
+		$pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	}
+	catch(PDOException $e){
+
+		die($e->getMessage());
+	}
+
+
+?>
