cc09cd6424e36f5babc59e66a2a6d15ab3ce2195
Andrew Crowley <44073467+andrewcrowley21@users.noreply.github.com>
2018-12-01 00:18:34 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', '');
+define('DBNAME', 'cs3500_imagedb');
+define('DBUSER', 'testuser');
+define('DBPASS', 'mypassword');
+define('DBCONNSTRING','mysql:dbname=cs3500_imagedb;charset=utf8mb4;');
+?>
